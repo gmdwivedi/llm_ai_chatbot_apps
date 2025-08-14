69498b37b15ec1ff025b2a2fd39b7335ce48bfd5
User <user@Users-Mac-mini.local>
2025-08-13 21:18:47 -0700

    ollama test script

---
+++ ollama_local_app/test_local_ollama.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+
+MODEL = "llama3.2"
+openai = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+query=[{"role": "user", "content": "What is 2 * 2?"}]
+
+response = openai.chat.completions.create(
+ model=MODEL,
+ messages=query
+)
+print("Query asked:" + query[0].get('content'))
+print(response.choices[0].message.content)
